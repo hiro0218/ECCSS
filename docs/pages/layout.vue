e63f139a3060046e6f63f39f8976ea501b8709eb
hiro0218 <hiro0218@gmail.com>
2018-11-21 23:40:33 +0900

    fix: add columns

--- docs/pages/layout.vue
+++ docs/pages/layout.vue
@@ -1,7 +1,32 @@
 <template>
-  <div>
-    Layout
-  </div>
+  <section class="p-layout">
+    <h1>Layout</h1>
+
+    <h2>o-columns</h2>
+    <div class="o-columns">
+      <div class="column">auto</div>
+      <div class="column">auto</div>
+      <div class="column">auto</div>
+      <div class="column">auto</div>
+    </div>
+
+    <div class="o-columns">
+      <div class="column is-full">full</div>
+    </div>
+
+    <div class="o-columns">
+      <div class="column is-half">half</div>
+      <div class="column">auto</div>
+    </div>
+
+    <div class="o-columns">
+      <div class="column is-one-quarter">one-quarter</div>
+      <div class="column">auto</div>
+      <div class="column">auto</div>
+      <div class="column">auto</div>
+    </div>
+
+  </section>
 </template>
 
 <script>
@@ -10,5 +35,22 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+.p-layout {
+  .o-columns {
+    margin-bottom: 2rem;
+    text-align: center;
+  }
+
+  .column {
+    background: #eee;
+    padding: 1rem;
+
+    &.is-full,
+    &.is-half,
+    &.is-one-quarter {
+      background: #aaa;
+    }
+  }
+}
 </style>
